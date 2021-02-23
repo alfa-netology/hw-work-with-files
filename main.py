d5d2dd54175c79d22472f08f7e917f781421de89
Alexsander Uglin <uglin76@mail.ru>
2021-02-24 00:40:08 +0300

    feat: Replace get_cook_book() on create_dict_from_file

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import modules.own_fucntions as functions
 
 # задача 1
 file = 'source/recipes.txt'
-cook_book = functions.get_cook_book(file)
+cook_book = functions.create_dict_from_file(file)
 
 # задача 2
-dishes = ('Фахитос', 'Омлет', 'Хинкали')
+dishes = ('фахитос', 'омлет', 'хинкали')
 person_count = 2
 print(functions.get_shop_list_by_dishes(dishes, person_count, cook_book))
 
--- modules/own_fucntions.py
+++ modules/own_fucntions.py
@@ -1,36 +1,60 @@
 import os
 
-def get_cook_book(file_):
-    """
-    возвращает словарь с рецептами блюд из файла
-    args:
-        file_(str) : путь к файлу с рецептами
-    return:
-        result(dict): словарь с рецептами блюд
-    """
+# def get_cook_book(file_):
+#     """
+#     возвращает словарь с рецептами блюд из файла
+#     args:
+#         file_(str) : путь к файлу с рецептами
+#     return:
+#         result(dict): словарь с рецептами блюд
+#     """
+#     result = {}
+#     dish = ''
+#     ingredient_quantity = 0
+#     with open(file_, 'r', encoding='utf-8') as f:
+#         data = f.readlines()
+#
+#     for line in data:
+#         line = line.strip()
+#         if line != '':
+#             if not line.isdigit() and '|' not in line:
+#                 dish = line
+#                 result[dish] = []
+#             elif '|' not in line:
+#                 ingredient_quantity = int(line)
+#
+#             elif '|' in line and ingredient_quantity > 0:
+#                 ingredient_name, quantity, measure = line.split('|')
+#                 result[dish].append({
+#                     'ingredient_name': ingredient_name.strip(),
+#                     'quantity': int(quantity),
+#                     'measure': measure,
+#                 })
+#                 ingredient_quantity -= 1
+#
+#     if len(result) > 0:
+#         return result
+#     else:
+#         return False
+
+def create_dict_from_file(file_name):
+    """Функция чтения файла + создание словаря нужного формата"""
     result = {}
-    dish = ''
-    ingredient_quantity = 0
-    with open(file_, 'r', encoding='utf-8') as f:
-        data = f.readlines()
-
-    for line in data:
-        line = line.strip()
-        if line != '':
-            if not line.isdigit() and '|' not in line:
-                dish = line
-                result[dish] = []
-            elif '|' not in line:
-                ingredient_quantity = int(line)
-
-            elif '|' in line and ingredient_quantity > 0:
-                ingredient_name, quantity, measure = line.split('|')
-                result[dish].append({
-                    'ingredient_name': ingredient_name.strip(),
+    with open(file_name, encoding='utf8') as file_work:
+        for line in file_work:
+            dish_name = line.lower().strip()
+            counter = int(file_work.readline())
+            list_of_ingredient = []
+            for i in range(counter):
+                ingredient_name, quantity, measure = file_work.readline().lower().strip().split(' | ')
+                temp_dict = {
+                    'ingredient_name': ingredient_name,
                     'quantity': int(quantity),
                     'measure': measure,
-                })
-                ingredient_quantity -= 1
+                }
+                list_of_ingredient.append(temp_dict)
+            result[dish_name] = list_of_ingredient
+            file_work.readline()
 
     if len(result) > 0:
         return result
