04851a7d543ddedd2b7fccba4c4fc6f10c268e56
Alexsander Uglin <uglin76@mail.ru>
2021-02-23 17:26:40 +0300

    Add function get_shop_list_by_dishes

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ file = 'source/recipes.txt'
 
 # задача 1
 cook_book = functions.get_cook_book(file)
-print(cook_book)
+
+dishes = ['Фахитос', 'Омлет']
+person_count = 2
+# задача 2
+print(functions.get_shop_list_by_dishes(dishes, person_count, cook_book))
+
--- modules/own_fucntions.py
+++ modules/own_fucntions.py
@@ -24,8 +24,8 @@ def get_cook_book(file_):
             elif '|' in line and ingredient_quantity > 0:
                 ingredient_name, quantity, measure = line.split('|')
                 result[dish].append({
-                    'ingredient_name': ingredient_name,
-                    'quantity': quantity,
+                    'ingredient_name': ingredient_name.strip(),
+                    'quantity': int(quantity),
                     'measure': measure,
                 })
                 ingredient_quantity -= 1
@@ -34,3 +34,26 @@ def get_cook_book(file_):
         return result
     else:
         return False
+
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for item in cook_book[dish]:
+                ingredient_name = item['ingredient_name']
+                if ingredient_name in result:
+                    quantity = item['quantity'] + result[ingredient_name]['quantity']
+                else:
+                    quantity = item['quantity']
+
+                result[ingredient_name] = {
+                    'measure': item['measure'],
+                    'quantity': quantity,
+                }
+        else:
+            print(f'В базе нет рецепта {dish}')
+
+    for key in result.keys():
+        result[key]['quantity'] *= person_count
+
+    return result
